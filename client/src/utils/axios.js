0213b5ea35d9ae8db8b7818c734cfff8fd9f7823
Prakhar Shukla <praakharshukla@4004gmail.com>
2026-06-02 22:48:54 +0530

    Fix User local backend url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URLL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URLL || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
